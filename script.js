074d1e187bbd422882d841d9c1847411f262af8a
safal-t <safaltimalsena0@gmail.com>
2025-03-21 05:55:16 +1100

    feat: Add operate function to handle operations based on operator and two numbers

--- script.js
+++ script.js
@@ -4,4 +4,25 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
+const operate = (firstNumber, operator, secondNumber) => {
+    switch (operator) {
+        case "+":
+            return add(firstNumber, secondNumber)
+        case "-":
+            return subtract(firstNumber, secondNumber)
+        case "*":
+            return multiply(firstNumber, secondNumber)
+        case "/":
+            return divide(firstNumber, secondNumber)
+        default:
+            return "Invalid operator"
+    }
+}
+
 // DECLARE VARIABLES
+let firstNumber;
+let secondNumber;
+let operator;
+
+
+
